9b35d271de6fd4c0e0d2afe789ead2c40f7d815e
RebirthX-ray <138247544+RebirthX-ray@users.noreply.github.com>
2023-07-01 01:21:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
   // Video source URL from the admin's Google Drive
-  var videoSource = 'https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive.jpg';
+  var videoSource = 'https://youtu.be/ojnA6bJ6Nwg';
   
   var player = document.getElementById('player');
   player.src = videoSource;
