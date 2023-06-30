f755ef15e9e735a8187b2f57112feab34be25baf
RebirthX-ray <138247544+RebirthX-ray@users.noreply.github.com>
2023-07-01 00:05:47 +0200

    Delete api.java

--- api.java
+++
@@ -1,9 +0,0 @@
-function listFiles() {
-  gapi.client.drive.files.list({
-    pageSize: 10,
-    fields: 'nextPageToken, files(id, name)',
-  }).then(function(response) {
-    var files = response.result.files;
-    // Process the files accordingly
-  });
-}
