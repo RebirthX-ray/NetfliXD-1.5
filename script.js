731fc9155d4a8169b6c12e90877eb9f3190f98f9
RebirthX-ray <138247544+RebirthX-ray@users.noreply.github.com>
2023-07-01 01:05:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
   // Video source URL from the admin's Google Drive
-  var videoSource = 'https://drive.google.com/uc?export=download&id=YOUR_VIDEO_ID';
+  var videoSource = 'https://drive.google.com/drive/folders/1P9C8pVIsrcD0k8ygR05sbJpOjP5EeURJ';
   
   var player = document.getElementById('player');
   player.src = videoSource;
