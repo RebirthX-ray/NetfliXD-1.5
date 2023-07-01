806dc2e83109e0d5717061b6c7cc95f01611efba
RebirthX-ray <138247544+RebirthX-ray@users.noreply.github.com>
2023-07-01 18:46:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function playVideo(filename) {
-  var player = document.getElementById("player");
-  player.innerHTML = "<video width='800' height='450' controls><source src='" + filename + "' type='video/mp4'></video>";
-}
+window.onload = function() {
+  var videoPlayer = document.getElementById('videoPlayer');
+
+  videoPlayer.addEventListener('play', function() {
+    // Perform any actions when the video starts playing
+    console.log('Video started playing');
+  });
+
+  videoPlayer.addEventListener('pause', function() {
+    // Perform any actions when the video is paused
+    console.log('Video paused');
+  });
+
+  videoPlayer.addEventListener('ended', function() {
+    // Perform any actions when the video ends
+    console.log('Video ended');
+  });
+};
